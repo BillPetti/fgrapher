d3a1c58afdbf67a6f87273a48b5caf2572222ed8
Bill Petti <billpetti@gmail.com>
2016-11-18 15:31:34 -0500

    updated FGQuery

--- R/FGQuery.R
+++ R/FGQuery.R
@@ -6,33 +6,30 @@
 #' \dontrun{FGQuery(query)}
 
 FGQuery = function(query) {
-
+  
   if (!exists("con")) {
-  fg_username <- readline("Please provide your username for the FanGraphs database: ")
-  assign("fg_username", fg_username, envir = .GlobalEnv)
-  
-  fg_password <- readline("Please provide your password for the FanGraphs database: ")
-  assign("fg_password", fg_password, envir = .GlobalEnv)
-  
-  con = dbConnect(RMySQL::MySQL(), dbname = "tht", username = fg_username, password = fg_password, host = "db.fangraphs.com")
-  assign("con", con, envir = .GlobalEnv)
-  
-  # player ids
-  ids <- FGQuery('select PlayerId, concat(firstname," ",lastname) as Name from player_info')
-  assign("ids", ids, envir = .GlobalEnv)
-  
-  #umpire ids
-  uids <- FGQuery('select distinct UName, UmpireId from umpires_daily_batting_full_pfx')
-  assign("uids", uids, envir = .GlobalEnv)
-  
-  data = dbGetQuery(con, query)
-  cons = dbListConnections(MySQL())
-  for(con in cons) {dbDisconnect(con)}
-  return(data)
-}
+    fg_username <- readline("Please provide your username for the FanGraphs database: ")
+    assign("fg_username", fg_username, envir = .GlobalEnv)
+    
+    fg_password <- readline("Please provide your password for the FanGraphs database: ")
+    assign("fg_password", fg_password, envir = .GlobalEnv)
+    
+    con <- dbConnect(RMySQL::MySQL(), dbname = "tht", username = fg_username, password = fg_password, host = "db.fangraphs.com")
+    
+    assign("con", con, envir = .GlobalEnv)
+    
+    # player ids
+    ids <- FGQuery('select PlayerId, concat(firstname," ",lastname) as Name from player_info')
+    assign("ids", ids, envir = .GlobalEnv)
+    
+    #umpire ids
+    uids <- FGQuery('select distinct UName, UmpireId from umpires_daily_batting_full_pfx')
+    assign("uids", uids, envir = .GlobalEnv)
+    
+    data = dbGetQuery(con, query)
+    return(data)
+  }
   else 
-  data = dbGetQuery(con, query)
-  cons = dbListConnections(MySQL())
-  for(con in cons) {dbDisconnect(con)}
+    data = dbGetQuery(con, query)
   return(data)
 }
